76513c4bf534abe630ae39b5f12c1de476f41de3
Siva Satya Sai <sivasatyasai217@gmail.com>
2024-09-04 01:12:20 +0530

    First Commit

---
+++ BookStore/src/main/java/com/mycompany/bookstore/BookStore.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookstore;
+
+/**
+ *
+ * @author HP
+ */
+public class BookStore {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+        //this.dispose();
+    }
+}
